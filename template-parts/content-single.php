82fb1e357e92c630e8b0e94a34b18f58fbb50385
tashenoff <kail.dead.kd@gmail.com>
2023-03-31 11:36:56 -0400

    .

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-12 gap-4">
-	<div class="col-span-9 ">
+	<div class="<? 	if (in_category(4)) { echo 'col-span-9';} else { echo 'col-span-12';} ?>">		
 
 		<?get_template_part('template-parts/product', 'description');?>
 		<?get_template_part('template-parts/product', 'advantage');?>
@@ -25,7 +25,7 @@
 
 
 		<?php
-		if (in_category(6)) {
+		if (in_category(4)) {
 			get_template_part('template-parts/sidebar', 'banner');
 		}
 		?>
